0e6d94ec5b5a2d7674cb14b6e7242ab69b741477
Cheney <Cheney_Wang@aliyun.com>
2019-07-29 10:45:48 +0800

    project init

---
+++ dev/mycrawler/src/main/java/store/acheng/mycrawler/Application.java
@@ -0,0 +1,11 @@
+package store.acheng.mycrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
